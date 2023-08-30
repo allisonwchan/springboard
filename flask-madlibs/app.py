7e34cafbf39c0852841bf2b83fdf0b52c2ab0c9e
allisonwchan <allison.wl.chan@gmail.com>
2023-08-30 11:39:34 -0700

    add html template inheritance and more stories

--- flask-madlibs/app.py
+++ flask-madlibs/app.py
@@ -1,19 +1,38 @@
 from flask import Flask, request, render_template
-from stories import story
-# from flask_debugtoolbar import DebugToolbarExtension
+from stories import stories
 
 app = Flask(__name__)
 
 @app.route('/')
-def form():
-    """Show Madlibs form on homepage."""
+def get_story():
+    """Let user choose story template."""
+
+    return render_template("base.html", stories=stories.values())
+
+@app.route("/words")
+def get_words():
+    """Generate and show form to ask words."""
+
+    story_id = request.args["story_id"]
+    story = stories[story_id]
 
     prompts = story.prompts
-    return render_template("home.html", prompts=prompts)
 
-@app.route('/story')
+    return render_template("words.html",
+                           story_id=story_id,
+                           title=story.title,
+                           prompts=prompts)
+
+
+@app.route("/story")
 def show_story():
-    """Show created story."""
+    """Show story result."""
+
+    story_id = request.args["story_id"]
+    story = stories[story_id]
 
     text = story.generate(request.args)
-    return render_template("story.html", text=text)
+
+    return render_template("story.html",
+                           title=story.title,
+                           text=text)
--- flask-madlibs/stories.py
+++ flask-madlibs/stories.py
@@ -1,13 +1,15 @@
 """Madlibs Stories."""
 
-
 class Story:
     """Madlibs story.
 
-    To  make a story, pass a list of prompts, and the text
-    of the template.
+    To  make a story, pass a code, a title, a list of prompts,
+    and the text of the template.
 
-        >>> s = Story(["noun", "verb"],
+        >>> s = Story(
+        ...     "simple",
+        ...     "A Simple Tale",
+        ...     ["noun", "verb"],
         ...     "I love to {verb} a good {noun}.")
 
     To generate text from a story, pass in a dictionary-like thing
@@ -18,9 +20,11 @@ class Story:
         'I love to eat a good mango.'
     """
 
-    def __init__(self, words, text):
+    def __init__(self, code, title, words, text):
         """Create story with words and template text."""
 
+        self.code = code
+        self.title = title
         self.prompts = words
         self.template = text
 
@@ -33,16 +37,24 @@ class Story:
             text = text.replace("{" + key + "}", val)
 
         return text
-
-
+    
 # Here's a story to get you started
 
-
-story = Story(
+story1 = Story(
+    "history",
+    "A History Tale",
     ["place", "noun", "verb", "adjective", "plural_noun"],
     """Once upon a time in a long-ago {place}, there lived a
        large {adjective} {noun}. It loved to {verb} {plural_noun}."""
 )
 
-ans = {"verb": "eat", "noun": "mango", "place": "china", "adjective": "hot", "plural_noun": "cats"}
-print(story.generate(ans))
+
+story2 = Story(
+    "omg",
+    "An Excited Adventure",
+    ["noun", "verb"],
+    """OMG!! OMG!! I love to {verb} a {noun}!"""
+)
+
+# Make dict of {code:story, code:story, ...}
+stories = {s.code: s for s in [story1, story2]}
