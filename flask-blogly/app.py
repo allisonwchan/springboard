69cd07cc7c02d167531deb98bff18e849a3e425f
allisonwchan <allison.wl.chan@gmail.com>
2023-09-29 02:22:06 -0700

    add new model

--- flask-blogly/app.py
+++ flask-blogly/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, flash
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db, User
+from models import db, connect_db, User, Post
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///blogly"
@@ -61,7 +61,8 @@ def users_show(user_id):
     """Show a page with info on a specific user"""
 
     user = User.query.get_or_404(user_id)
-    return render_template('user-info.html', user=user)
+    posts = Post.query.filter_by(user_id=user_id).all()
+    return render_template('user-info.html', user=user, posts=posts)
 
 
 @app.route('/users/<int:user_id>/edit')
@@ -96,3 +97,77 @@ def users_destroy(user_id):
     db.session.commit()
 
     return redirect("/users")
+
+
+##############################################################################
+# Post route
+
+
+@app.route('/users/<int:user_id>/posts/new')
+def show_post_form(user_id):
+    """Show form for adding a new post."""
+
+    user = User.query.get_or_404(user_id)
+    return render_template('new-post.html', user=user)
+
+
+@app.route('/users/<int:user_id>/posts/new', methods=['POST'])
+def post_add(user_id):
+    """Handle form submission for adding a post."""
+
+    user = User.query.get_or_404(user_id)
+
+    new_post = Post(
+        title=request.form['title'],
+        content=request.form['content'],
+        user=user
+    )
+
+    db.session.add(new_post)
+    db.session.commit()
+    flash(f"Post '{new_post.title}' added.")
+
+    return redirect(f'/users/{user_id}')
+
+
+@app.route('/posts/<int:post_id>')
+def show_post(post_id):
+    """Show a post."""
+
+    post = Post.query.get_or_404(post_id)
+    return render_template('post-detail.html', post=post)
+
+
+@app.route('/posts/<int:post_id>/edit')
+def show_edit_post(post_id):
+    """Show form to edit a post."""
+
+    post = Post.query.get_or_404(post_id)
+    return render_template('edit-post.html', post=post)
+
+
+@app.route('/posts/<int:post_id>/edit', methods=['POST'])
+def update_post(post_id):
+    """Handle editing of a post."""
+
+    post = Post.query.get_or_404(post_id)
+    post.title = request.form['title']
+    post.content = request.form['content']
+    
+    db.session.add(post)
+    db.session.commit()
+    flash(f"Post '{post.title}' edited.")
+
+    return redirect(f'/users/{post.user_id}')
+
+
+@app.route('/posts/<int:post_id>/delete', methods=['POST'])
+def delete_post(post_id):
+    """Handle deleting of a post."""
+
+    post = Post.query.get_or_404(post_id)
+    db.session.delete(post)
+    db.session.commit()
+    flash(f"Post '{post.title} deleted.")
+
+    return redirect(f'/users')
