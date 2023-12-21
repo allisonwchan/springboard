4c1da5840e2e494535ad8c67db85dfc4d80770f0
allisonwchan <allison.wl.chan@gmail.com>
2023-12-20 18:08:44 -0800

    add express biztime exercise

---
+++ express-biztime/app.js
@@ -0,0 +1,38 @@
+/** BizTime express application. */
+
+
+const express = require("express");
+
+const app = express();
+const companiesRoutes = require('./routes/companies');
+const invoicesRoutes = require('./routes/invoices');
+const ExpressError = require("./expressError")
+
+app.use(express.json());
+app.use("/", companiesRoutes);
+app.use("/", invoicesRoutes);
+
+/** 404 handler */
+
+app.use(function(req, res, next) {
+  const err = new ExpressError("Not Found", 404);
+  return next(err);
+});
+
+/** general error handler */
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+
+  return res.json({
+    error: err,
+    message: err.message
+  });
+});
+
+// uncomment if running locally
+app.listen(3000, function () {
+  console.log("Listening on 3000");
+});
+
+module.exports = app;
